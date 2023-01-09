90414902ec938458d3570aa2bf125a089dd06bce
Reginaldo Castro <reginaldo.costa@gruporedeamazonica.com.br>
2023-01-09 10:23:48 -0400

    Criando feeds para posts

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostFeed
 
 app_name = 'blog'
 
@@ -9,4 +10,5 @@ urlpatterns = [
     #path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/', LatestPostFeed(), name='post_feed')
 ]
